b14b73aa0e8455b6830f8d47fb7a6d2a804915c1
Kelvyloo <kjlu11823@gmail.com>
2019-01-24 16:42:53 -0500

    Init commit fork_and_exec files to separate those utilities

---
+++ fork_and_exec.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h> /* pid_t                   */
+#include <sys/wait.h>  /* waitpid(), WEXITSTATUS()*/
+#include <unistd.h>    /* execvp(), fork()        */
+
+#include "parse.h" /* Task struct */
+
+int execute_cmd(Task *tasks, int ntasks) 
+{
+    unsigned int t;
+
+    for (t = 0; t < ntasks; t++) {
+        pid_t pid;
+
+        pid = fork();
+
+        if (pid < 0) {
+            fprintf(stderr, "error -- failed to fork()");
+            exit(EXIT_FAILURE);
+        }
+
+        if (pid > 0) {
+            /* only executed by the PARENT process */
+            int child_ret;
+            waitpid(pid, &child_ret, 0);
+        } 
+        
+        else {
+            if(execvp(tasks[t].cmd, tasks[t].argv)) {
+                printf("Failed to exec %s\n", tasks[t].cmd);
+                exit(EXIT_FAILURE);
+            }
+        }
+    }
+
+    return EXIT_SUCCESS;
+}
---
+++ fork_and_exec.h
@@ -0,0 +1,8 @@
+#ifndef _fork_and_exec_h_
+#define _fork_and_exec_h_
+
+#include "parse.h"
+
+int execute_cmd(Task *tasks, int ntasks);
+
+#endif
