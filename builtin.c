fb12c4dc942f85bfc7b8789cf5deaaa5e874f460
Kelvyloo <kjlu11823@gmail.com>
2019-01-30 16:48:44 -0500

    Implemented shell built-in which command

--- builtin.c
+++ builtin.c
@@ -25,11 +25,45 @@ int is_builtin (char* cmd)
     return 0;
 }
 
+void find_filepath(Task T)
+{
+    char *PATH;
+    char *tmp;
+    char *dir;
+    char probe[PATH_MAX];
+
+    PATH = strdup(getenv("PATH"));
+
+    for (tmp = PATH; ; tmp = NULL) {
+        dir = strtok(tmp, ":");
+
+        if (!dir) { break; }
+
+        strncpy(probe, dir, PATH_MAX);
+        strncat(probe, "/", PATH_MAX);
+        strncat(probe, *(T.argv + 1) , PATH_MAX);
+
+        if (access(probe, X_OK) == 0) {
+            printf("%s\n", probe);
+            break;
+        }
+    }
+
+    free(PATH);
+}
 
 void builtin_execute (Task T)
 {
     if (!strcmp (T.cmd, "exit")) {
         exit (EXIT_SUCCESS);
+    }
+    if (!strcmp (T.cmd, "which")) {
+        if (is_builtin(*(T.argv + 1))) {
+            printf("%s: shell built-in command\n", *(T.argv + 1));
+        }
+        else {
+            find_filepath(T);
+        }
     }
     else {
         printf ("pssh: builtin command: %s (not implemented!)\n", T.cmd);
