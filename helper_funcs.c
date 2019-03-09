6ab11c5938b3d744e15ba37b25a6e3a56d9a6cc6
Kelvyloo <kjlu11823@gmail.com>
2019-03-09 14:35:27 -0500

    Added API to compare child_pid to jobs to check for job completion

--- helper_funcs.c
+++ helper_funcs.c
@@ -8,13 +8,21 @@
 #include <fcntl.h>     /* open()      */
 #include "helper_funcs.h"
 
-void init_job(Job *job)
+static void init_job(Job *job)
 {
     job->pid = NULL;
     job->name = NULL;
     job->npids = 0;
     job->pgid = 0;
     job->status = 0;
+}
+
+void init_jobs(Job *jobs, int num_jobs)
+{
+    int i;
+
+    for (i = 0; i < num_jobs; i++)
+        init_job(&jobs[i]);
 }
 
 void create_job(Job *job, Parse *P, pid_t pgid)
@@ -53,16 +61,38 @@ void set_fg_pgid(pid_t pgid)
     signal (SIGTTOU, old);
 }
 
+int is_job_done(pid_t child_pid, Job *jobs, int num_jobs, int *killed)
+{
+    int i, j;
+    int skip = 0;
+    int finished_job;
+
+    for (i = 0; i < num_jobs; i++) {
+        for (j = 0; j < jobs[i].npids; i++) {
+            if (child_pid == jobs[i].pid[j]) {
+                killed[i]++;
+                finished_job = (killed[i] == jobs[i].npids) ? i+1 : 0;
+                skip = 1;
+                break;
+            }
+        }
+        if (skip)
+            break;
+    }
+
+    return finished_job;
+}
+
 void print_job_info(int job_num, Job *job, int done)
 {
     if (job->status == BG) {
         if (done)
-            printf("[%d]+ Done \t%s\n", job_num, job->name);
+            printf("[%d]+ Done \t%s\n", job_num+1, job->name);
 
         else {
             int t;
 
-            printf("[%d] ", job_num);
+            printf("[%d] ", job_num+1);
 
             for (t = 0; t < job->npids; t++)
                 printf("%d ", job->pid[t]);
--- helper_funcs.h
+++ helper_funcs.h
@@ -18,13 +18,15 @@ typedef struct {
     JobStatus status;
 } Job;
 
-void init_job(Job *job);
+void init_jobs(Job *jobs, int num_jobs);
 
 void create_job(Job *job, Parse *P, pid_t pgid);
 
 void destroy_job(Job *job);
 
 void set_fg_pgid(pid_t pgid);
+
+int is_job_done(pid_t child_pid, Job *jobs, int num_jobs, int *killed);
 
 void print_job_info(int job_num, Job *job, int done);
 
