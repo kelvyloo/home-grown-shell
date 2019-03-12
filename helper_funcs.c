cf037b3f85f262e41913d47f04ecb25a4ee14935
Kelvyloo <kjlu11823@gmail.com>
2019-03-11 20:24:23 -0400

    sigcont sets status of job appropriately

--- helper_funcs.c
+++ helper_funcs.c
@@ -242,8 +242,7 @@ void sigcont_cmd(char **argv, int fg)
 
     fprintf(stdout, "%s\n", jobs[target_job].name);
 
-    //jobs[target_job].status = FG;
-    jobs[target_job].status = (fg) ? FG : BG;
+    jobs[target_job].status = (fg) ? FG : STOPPED;
 
     kill(-jobs[target_job].pgid, SIGCONT);
 
